b127f119711101ec8359966646c0189db2d0ebf0
Durgesh Soni <durgeshsoni1082003@gmail.com>
2022-10-11 21:18:36 +0530

    food app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
-import Header  from "./FoodApp/Layouts/Header";
+import Cart from "./FoodApp/Cart/Cart";
+import Header from "./FoodApp/Layouts/Header";
 import Meals from "./FoodApp/Meals/Meals";
+import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
 
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <div >
+    <CartProvider>
       {/* <Demo/> */}
-      <Header/>
-      <main >
-        <Meals/>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
       </main>
-    </div>
+    </CartProvider>
   );
 };
 
